ceff8777e69702322161fd9f1020cd2eac35d3b1
Sergey Dedkov <dsv.mail@yandex.ru>
2020-10-07 22:07:06 +0300

    build

---
+++ packages/shared-todos-web/build/precache-manifest.e99edc99974b4caf6498cd6c77cee001.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4cf227267470479a46e620e7fdc6d0",
+    "url": "/shared-todos/index.html"
+  },
+  {
+    "revision": "614d01d2ca482b88be8b",
+    "url": "/shared-todos/static/css/main.c2aa6d86.chunk.css"
+  },
+  {
+    "revision": "94349f2b5160af6ff5ad",
+    "url": "/shared-todos/static/js/2.1bcbc52f.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/shared-todos/static/js/2.1bcbc52f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "614d01d2ca482b88be8b",
+    "url": "/shared-todos/static/js/main.9e04f56d.chunk.js"
+  },
+  {
+    "revision": "396202404de7d8872e17",
+    "url": "/shared-todos/static/js/runtime-main.1b8a44a6.js"
+  }
+]);
